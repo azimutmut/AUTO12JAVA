12d13dee45deb145d67b9ef5a5d096f1b6632e63
azimutmut <azimutmut@gmail.com>
2025-02-16 03:25:34 +0100

    lesson 17 task 3 real final

--- LessonsAndersen/src/test/java/lesson17/task3/Alerts.java
+++ LessonsAndersen/src/test/java/lesson17/task3/Alerts.java
@@ -10,15 +10,18 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import static org.testng.Assert.assertEquals;
 import org.testng.annotations.AfterTest;
 import static org.testng.Assert.assertTrue;
 import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class Alerts {
-
     private static class Locators {
         private static final By btn = By.id("AlertButton");
+        private static final String alertText1 = "You have called alert!";
+        private static final String getAlertText2 = "Are you sure you want to apply the discount?";
+        private static final String getAlertText3 = "Here you may describe a reason why you are cancelling your registration (or leave this field empty).";
         private static final By doubleClickLocator = By.xpath("//button[text()='Get Discount']");
         private static final By checkTextTest = By.xpath("//button[@data-test-id='PromptButton']");
     }
@@ -50,10 +53,13 @@ public class Alerts {
         driver.findElement(By.xpath("//div[text()='Actions, Alerts & Iframes']")).click();
         Thread.sleep(1000);
 
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[@title='Finish your registration']")));
         wait.until(ExpectedConditions.visibilityOfElementLocated(Locators.btn));
         WebElement btn = driver.findElement(By.id("AlertButton"));
         btn.click();
         Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        alert.getText();
+        assertEquals(alert.getText(), Locators.alertText1);
         alert.accept();
         WebElement actualText1Locator = driver.findElement(By.xpath("//span[text()='Congratulations, you have successfully enrolled in the course!']"));
         String expectedText = "Congratulations, you have successfully enrolled in the course!";
@@ -62,19 +68,23 @@ public class Alerts {
 
         actions.doubleClick(driver.findElement(Locators.doubleClickLocator)).perform();
         alert = wait.until(ExpectedConditions.alertIsPresent());
+        alert.getText();
+        assertEquals(alert.getText(), Locators.getAlertText2);
         alert.accept();
         WebElement actualDoubleClickTextLocator = driver.findElement(By.xpath("//span[contains(text(),'You received a 10% discount')]"));
         String expectedText2 = "You received a 10% discount on the second course.";
         String actualText2 = actualDoubleClickTextLocator.getText();
         Assert.assertEquals(actualText2, expectedText2, "The search result message is incorrect!");
 
-        actions.doubleClick(driver.findElement(Locators.checkTextTest)).perform();
+        actions.contextClick(driver.findElement(Locators.checkTextTest)).perform();
+        Alert alert3 = driver.switchTo().alert();
+        alert3.getText();
+        assertEquals(alert3.getText(), Locators.getAlertText3);
         String textTest = "Test";
-        alert.sendKeys(textTest);
-        alert.accept();
-        WebElement locatorOfFulltext = driver.findElement(By.xpath("//span[text()='Your course application has been cancelled. Reason: test']"));
+        alert3.sendKeys(textTest);
+        alert3.accept();
+        WebElement locatorOfFulltext = driver.findElement(By.xpath("//span[text()='Your course application has been cancelled. Reason: Test']"));
         String actualText3 = locatorOfFulltext.getText();
         assertTrue(actualText3.contains(textTest));
-
     }
 }
